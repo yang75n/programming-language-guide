da55e12f83b8293aa45a8c3db83a70978492cfc7
Qiwen <qiwen@62.com>
2020-02-14 14:56:10 +0800

    add C++/Build

---
+++ C++/12_Build/cmake/demo3/Build/CMakeFiles/Demo3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Demo3.dir/mathfunctions.cpp.o"
+  "CMakeFiles/Demo3.dir/main.cpp.o"
+  "Demo3.pdb"
+  "Demo3"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Demo3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ C++/12_Build/cmake/demo4/Build/CMakeFiles/Demo4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Demo4.dir/main.cpp.o"
+  "Demo4.pdb"
+  "Demo4"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Demo4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
